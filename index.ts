62230210fb48c99984737d432defe6f9cbde9769
Ilja B <49001925+iljucha@users.noreply.github.com>
2020-03-04 21:09:50 +0100

    Delete index.ts

--- index.ts
+++
@@ -1,7 +0,0 @@
-export default function hash(s: string): number {
-    let h: number, i = 0, length = s.length
-    for (i; i < length; i++) {
-        h = Math.imul(31, h) + s.charCodeAt(i) | 0
-    }
-    return h
-}
