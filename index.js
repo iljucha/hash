41b687df48a1a878d5513fc24cc83c37087d9ed1
Ilja B <49001925+iljucha@users.noreply.github.com>
2020-06-19 13:35:09 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+import Assert from "@iljucha/assert"
 /**
  * Converts String into hash
  * @param {string} s - Input String
  */
 export default function hash(s) {
+    Assert.type(s, "hash string input", "string")
     let h, i = 0, length = s.length
     for (i; i < length; i++) {
         h = Math.imul(31, h) + s.charCodeAt(i) | 0
